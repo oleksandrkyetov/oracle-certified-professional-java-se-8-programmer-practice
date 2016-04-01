2c0ec6896b8cc8430a28294cbad3ecd4729b93ac
Oleksandr Kyetov <oleksandr.kyetov@gmail.com>
2016-04-02 01:02:05 +0300

    Chapter 3 finished

--- Chapter2DesignPatternsAndPrinciples/FunctionalInterfacesAndLambdas.java
+++ Chapter2DesignPatternsAndPrinciples/FunctionalInterfacesAndLambdas.java
@@ -1,21 +1,94 @@
+import java.util.function.Predicate;
+
 public class FunctionalInterfacesAndLambdas {
 
 	public static void main(final String... args) {
+		/*
+		 * Long lambda used
+		 * Non conditional printing method is used
+		 */
+		toStringablePrint(
+			new Instance("instance1"),
+			(ToStringable toStringable) -> {
+				return "[" + toStringable.toString() + "] " + toStringable.getString();
+			}
+		);
 
+		/*
+		 * Short lambda used
+		 * Predicate returns true
+		 */
+		toStringablePrint(
+			new Instance("instance2"),
+			toStringable -> "[" + toStringable.toString() + "] " + toStringable.getString(),
+			toStringable -> toStringable.getString().contains("instance")
+		);
+
+		/*
+		 * Mixed lambda used
+		 * Predicate returns false
+		 */
+		toStringablePrint(
+			new Instance("3"),
+			(toStringable) -> "[" + toStringable.toString() + "] " + toStringable.getString(),
+			toStringable -> {
+				return toStringable.getString().contains("instance");
+			}
+		);
+	}
+
+	/**
+	 * Print ToStringable with a condition
+	 */
+	public static void toStringablePrint(final Instance instance, final ToStringer<ToStringable> toStringer, final Predicate<ToStringable> predicate) {
+		if (predicate.test(instance)) {
+			System.out.println("Printed with ToStringer: " + toStringer.toStringableToString(instance));
+		} else {
+			System.out.println("[ERROR] Not printed with ToStringer: " + toStringer.toStringableToString(instance));
+		}
+	}
+
+	/**
+	 * Print ToStringable
+	 */
+	public static void toStringablePrint(final Instance instance, final ToStringer<ToStringable> toStringer) {
+		// Method delegation used
+		toStringablePrint(
+			instance,
+			toStringer,
+			toStringable -> true
+		);
+
+		//System.out.println("Printed with ToStringer: " + toStringer.toStringableToString(instance));
 	}
 
 }
 
-class Instance {
+interface ToStringable {
 
-	public 
+	String getString();
+
+}
+
+class Instance implements ToStringable {
+
+	private final String string;
+
+	Instance(final String string) {
+		this.string = string;
+	}
+
+	@Override
+	public String getString() {
+		return string;
+	}
 
 }
 
 @FunctionalInterface
-interface Check {
+interface ToStringer<ToStringable> {
 
-	void test(final Instance instance);
+	String toStringableToString(final ToStringable toStringable);
 
 }
 
